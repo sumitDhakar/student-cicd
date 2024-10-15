05c6cac860c8526aa61844c94b8d8c274a6e0fad
sumit1213 <sumitd.dollop2gmail.com>
2024-10-15 14:17:22 +0530

    test-git

--- src/main/java/com/example/demo/entity/Attendance.java
+++ src/main/java/com/example/demo/entity/Attendance.java
@@ -1,14 +1,14 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.sql.Date;
 
 @Entity
 @Data
+@Setter
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Attendance {
@@ -16,14 +16,64 @@ public class Attendance {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public boolean isPresent() {
+        return present;
+    }
+
+    public void setPresent(boolean present) {
+        this.present = present;
+    }
+
     @ManyToOne
-    @JoinColumn(name = "student_id")
+    @JoinColumn(name = "student_id", nullable = false)
     private Student student;
 
     @ManyToOne
     @JoinColumn(name = "course_id")
     private Course course;
-
     private Date date;
     private String status;
+    private boolean present;
+
+
 }
--- src/main/java/com/example/demo/repo/AttendanceRepo.java
+++ src/main/java/com/example/demo/repo/AttendanceRepo.java
@@ -1,7 +1,13 @@
 package com.example.demo.repo;
 
 import com.example.demo.entity.Attendance;
+import lombok.Data;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AttendanceRepo extends JpaRepository<Attendance,Long> {
+
+    Optional<Attendance> findByStudentIdAndDate(Long studentId, Data date);
+
 }
